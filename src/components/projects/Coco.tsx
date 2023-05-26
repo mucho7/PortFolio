4b7cf2b21bdf24cf053d1f0b251ba7fc83b3b31a
yadon <mucho7@gmail.com>
2023-05-26 10:27:47 +0900

    Docs: 맡은 역할 추가

--- src/components/projects/Coco.tsx
+++ src/components/projects/Coco.tsx
@@ -19,6 +19,14 @@ function Coco() {
     url: "https://github.com/mucho7/CommonProject",
     content:
       "**코딩테스트를 준비하는 취준생을 위한 실시간 코드 공유 플랫폼**입니다. 기존 코딩 플랫폼의 단점을 보완하여 사용자 입장에서 더 빠르고 편리하게 공부를 진행할 수 있도록 돕기위해 기획한 서비스입니다.\n\n대규모 프로젝트에 처음으로 **React**를 도입하여 개인적으로 의미가 깊었습니다. **Redux**나 **styled-components**같은 기본적인 React라이브러리들을 활용했습니다.\n\n**WebRTC**를 활용하여 사용자들이 실시간 코드를 공유하고 코드를 그림으로 설명할 수 있게 **CanvasAPI**를 활용했습니다.\n\n다양한 **React Hook**에 대해 알 수 있었고 **MSA구조**를 처음으로 도입하며 배포에 대한 기본지식을 배울 수 있었습니다. 그 결과, SSAFY 내부평가에서 좋은 평가를 받아 **2등으로 우수상**을 수상했습니다.",
+    participation: [
+      "프론트엔드 팀장으로서 공통 컴포넌트 작성",
+      "MonacoEditor를 활용하여 VS code와 동일한 형식의 webIDE 제공",
+      "Markdown 형식의 게시글 작성 기능 구현",
+      "코드 하이라이팅 기능을 통해 가독성 증진 ",
+      "React가 처음인 팀원을 위해 스터디 조직",
+      "Figma를 사용해 와이어프레임 작성",
+    ],
     mainFunc: [
       { content: "실시간 코드 공유", isMine: false },
       { content: "Markdown형식의 게시글 본문", isMine: true },
--- src/components/projects/DongBang.tsx
+++ src/components/projects/DongBang.tsx
@@ -41,11 +41,23 @@ function DongBang() {
     url: "https://github.com/mucho7/",
     content:
       "면접 전 AI 역량검사를 시행하는 기업은 현재 1000개 이상이며 **개발자용 AI 역검** 이 출시되며 대비의 필요성이 커지고 있습니다.\n\n AI 역량검사는 연습을 통해 좋은 결과를 얻을 수 있지만 **지원자를 위한 서비스는 존재하지 않으며 시험 응시 후 정확한 결과를 확인하기 어렵습니다.**\n\n 동방역검은 AI 역량검사 지원자를 위한 서비스로 **AI 역량검사 전략게임을 연습하고 수치화된 게임 채점 결과와 영상 분석 결과를 제공**합니다.",
+    participation: [
+      "프론트엔드 팀장으로서 공통 컴포넌트 작성",
+      "모든 프론트엔드 코드 리뷰 및 피드백",
+      "4개의 게임 중 3개의 게임 구현",
+      "랜덤하게 문제를 제공하는 알고리즘 구현",
+      "TensorFlow.js를 활용해 응시자의 얼굴 인식 기능 구현",
+      "시험 페이지를 HOC 디자인 패턴으로 작성",
+      "Firebase를 활용한 회원 기능 구현",
+      "Kurento와 관련된 코드 TS로 전환 및 리팩토링",
+      "Wireframe, Flow chart, 시퀀스 다이어그램, 간트 차트 등 공용 문서 작성",
+    ],
     mainFunc: [
       { content: "전략게임 연습", isMine: true },
       { content: "게임별 세부 통계", isMine: false },
       { content: "종합 분석", isMine: false },
-      { content: "빅데이터 사주팔자", isMine: false },
+      { content: "빅데이터 사주팔자", isMine: true },
+      { content: "분석완료 알람기능", isMine: false },
     ],
     techStack: [
       {
